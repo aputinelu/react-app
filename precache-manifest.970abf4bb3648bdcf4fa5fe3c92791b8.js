cec7a8b58d7e31063b9930c206bde8c238ffe362
aputinelu <55539157+aputinelu@users.noreply.github.com>
2021-02-19 19:12:33 +0200

    Updates

---
+++ precache-manifest.970abf4bb3648bdcf4fa5fe3c92791b8.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc05932e57ff8b6eb2752e4057d44b05",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "1b888a252d256b9619c2",
+    "url": "/react-app/static/css/2.7de87acc.chunk.css"
+  },
+  {
+    "revision": "5dc3ccd2668fcc1a35e6",
+    "url": "/react-app/static/css/main.0f072b3d.chunk.css"
+  },
+  {
+    "revision": "1b888a252d256b9619c2",
+    "url": "/react-app/static/js/2.bd693efd.chunk.js"
+  },
+  {
+    "revision": "aaddf6e5ed8e3df778811353fd2abc8e",
+    "url": "/react-app/static/js/2.bd693efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc3ccd2668fcc1a35e6",
+    "url": "/react-app/static/js/main.9c31200a.chunk.js"
+  },
+  {
+    "revision": "b3a0d066667660c5e5cd",
+    "url": "/react-app/static/js/runtime-main.9282a729.js"
+  },
+  {
+    "revision": "29a6c5f2fbcf11303721d67bceeab5f5",
+    "url": "/react-app/static/media/1.29a6c5f2.jpg"
+  },
+  {
+    "revision": "a5af348a0fba596be6c2ab7b0b1af478",
+    "url": "/react-app/static/media/1.a5af348a.jpg"
+  },
+  {
+    "revision": "cbbfba69c5f30fa33d4cc9c8cab4f242",
+    "url": "/react-app/static/media/1.cbbfba69.jpg"
+  },
+  {
+    "revision": "cbe88be568ed779b765d37bc30e512c8",
+    "url": "/react-app/static/media/1.cbe88be5.jpg"
+  },
+  {
+    "revision": "de32827c690341063cc912de9045be0e",
+    "url": "/react-app/static/media/1.de32827c.jpg"
+  },
+  {
+    "revision": "cbe88be568ed779b765d37bc30e512c8",
+    "url": "/react-app/static/media/2.cbe88be5.jpg"
+  },
+  {
+    "revision": "767f0bbb7218666a1fe2fee8e7f83e1f",
+    "url": "/react-app/static/media/3.767f0bbb.jpg"
+  },
+  {
+    "revision": "4f2c23cc0e43b4213ed1b647f3723fa5",
+    "url": "/react-app/static/media/5.4f2c23cc.png"
+  },
+  {
+    "revision": "59b425fc8ce970fa33d8eed2f752d385",
+    "url": "/react-app/static/media/Adi.59b425fc.png"
+  },
+  {
+    "revision": "08a22e493f8dadda54f33897d82a86f1",
+    "url": "/react-app/static/media/Flaticon.08a22e49.svg"
+  },
+  {
+    "revision": "613c2987e800d292cf531d574996c1ed",
+    "url": "/react-app/static/media/Flaticon.613c2987.eot"
+  },
+  {
+    "revision": "cecbacb6e3488e9e7a434ce568d746ca",
+    "url": "/react-app/static/media/Flaticon.cecbacb6.ttf"
+  },
+  {
+    "revision": "ed1ba1def7330f7a90433f244f5b3a41",
+    "url": "/react-app/static/media/Flaticon.ed1ba1de.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "488306d50024a919fdcf12500887a2bb",
+    "url": "/react-app/static/media/hm.488306d5.png"
+  },
+  {
+    "revision": "1daf85b52296a4d16411259ea623b643",
+    "url": "/react-app/static/media/lamia.1daf85b5.png"
+  },
+  {
+    "revision": "1987a4a894fe1a8dc77c68c2f79f28df",
+    "url": "/react-app/static/media/logo.1987a4a8.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-app/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "1b8fdceb3ac53c78b3d8ff31face91ac",
+    "url": "/react-app/static/media/slide-1.1b8fdceb.jpg"
+  },
+  {
+    "revision": "1b8fdceb3ac53c78b3d8ff31face91ac",
+    "url": "/react-app/static/media/slide-22.1b8fdceb.jpg"
+  },
+  {
+    "revision": "1b8fdceb3ac53c78b3d8ff31face91ac",
+    "url": "/react-app/static/media/slide-4.1b8fdceb.jpg"
+  },
+  {
+    "revision": "1b8fdceb3ac53c78b3d8ff31face91ac",
+    "url": "/react-app/static/media/slide-5.1b8fdceb.jpg"
+  },
+  {
+    "revision": "ae7c1cb827fb9a7e581c1db75f5f5094",
+    "url": "/react-app/static/media/t4.ae7c1cb8.jpg"
+  }
+]);
